0549bf19b5110823ad67f3d6c5ef3b1e7776e9dd
NikosWork1 <148493746+NikosWork1@users.noreply.github.com>
2025-05-01 18:47:32 +0300

    Delete rest_impl directory

--- rest_impl/backend/routes/__init__.py
+++
@@ -1,5 +0,0 @@
-# Import blueprints to make them available when the package is imported
-from routes.auth import auth_bp
-from routes.menu import menu_bp
-from routes.order import order_bp
-from routes.reservation import reservation_bp
